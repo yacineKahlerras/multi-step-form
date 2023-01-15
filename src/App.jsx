605558c4d8018d69beb146f2dcbd19cc2f74056d
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-15 21:59:40 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import "./index.css";
+import "@/index.css";
+import navBgMobile from "@/assets/images/bg-sidebar-mobile.svg";
 
 function App() {
   const navHeaders = ["your info", "select plan", "add-ons", "summary"];
 
   const navLinksElements = navHeaders.map((stepTitle, index) => {
     return (
-      <li className="flex flex-col gap-2">
-        <span className="p-1 border">{index + 1}</span>
-        <div>
+      <li key={index} className="flex gap-2">
+        <span className="w-10 h-10 m-2 border grid content-center text-center rounded-full">
+          {index + 1}
+        </span>
+        <div className="flex-col gap-1 hidden lg:flex">
           <span>STEP {index + 1}</span>
           <span>{stepTitle}</span>
         </div>
@@ -16,10 +19,15 @@ function App() {
   });
 
   return (
-    <main className="min-h-screen grid items-center font-ubuntu">
+    <main className="min-h-screen grid justify-center font-ubuntu">
       <div className="flex flex-col">
-        <nav>
-          <ul className="">{navLinksElements}</ul>
+        <nav className="py-[3rem] relative">
+          <ul className="flex justify-center">{navLinksElements}</ul>
+          <img
+            className="absolute top-0 left-0 object-cover"
+            src={navBgMobile}
+            alt="nav-bg"
+          />
         </nav>
       </div>
     </main>
