a1da4d66b0961bdde2e111388283a16fb57ec81c
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-17 14:14:08 +0100

    ok

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,24 @@
 import "@/index.css";
 import navBgMobile from "@/assets/images/bg-sidebar-mobile.svg";
 import navBgDesktop from "@/assets/images/bg-sidebar-desktop.svg";
+import { useContext } from "react";
+import StepContext from "@/utils/contexts/stepContext";
 
 function Nav() {
   const navHeaders = ["your info", "select plan", "add-ons", "summary"];
+  const { step } = useContext(StepContext);
 
   const navLinksElements = navHeaders.map((stepTitle, index) => {
     return (
       <li key={index} className="flex gap-2">
-        <span className="w-10 h-10 m-2 border-2 grid content-center text-center rounded-full">
+        <span
+          className={`w-10 h-10 m-2 border-2 grid content-center text-center rounded-full 
+          ${
+            step != index + 1
+              ? ""
+              : "text-MarineBlue font-bold bg-PastelBlue border-PastelBlue"
+          }`}
+        >
           {index + 1}
         </span>
         <div className="flex-col gap-1 hidden lg:flex">
