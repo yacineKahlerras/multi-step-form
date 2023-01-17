b7239b58c8dd5339f8591bcf3b34e3efa3070cbb
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-17 20:50:10 +0100

    ok

--- src/components/PersonnalInfo/PersonnalInfo.jsx
+++ src/components/PersonnalInfo/PersonnalInfo.jsx
@@ -58,7 +58,7 @@ function PersonnalInfo() {
   return (
     <div>
       <div className="card-component">
-        <h1 className="text-xl font-bold mb-2">Personnal Info</h1>
+        <h1 className="card-header">Personnal Info</h1>
         <p className="text-CoolGray text-sm mb-3">
           Please provide your name, email address, and phone number.
         </p>
--- src/components/Plan/Plan.jsx
+++ src/components/Plan/Plan.jsx
@@ -4,7 +4,7 @@ function Plan(props) {
   return (
     <div>
       <div className="card-component">
-        <h1 className="text-xl font-bold mb-2">Personnal Info</h1>
+        <h1 className="card-header">Personnal Info</h1>
         <p className="text-CoolGray text-sm mb-3">
           Please provide your name, email address, and phone number.
         </p>
