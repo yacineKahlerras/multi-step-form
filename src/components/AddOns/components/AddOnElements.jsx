d7314a66068182226ca0a466c79129a075eb1cb1
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-20 14:59:18 +0100

    ok

--- src/components/AddOns/components/AddOnElements.jsx
+++ src/components/AddOns/components/AddOnElements.jsx
@@ -8,29 +8,27 @@ function AddOnElements(props) {
   const isYearly = billingPlan.billingPer === "year";
   const priceCoefficent = isYearly ? 10 : 1;
 
-  const freeMonthsElement = isYearly ? (
-    <span className=" font-medium text-MarineBlue text-xs">2 months free</span>
-  ) : (
-    ""
-  );
-
-  return addOnInfo.map((plan, index) => {
+  return addOnInfo.map((addon, index) => {
     const activePlanStyle =
-      billingPlan.name !== plan.name ? "" : "border-PurplishBlue bg-Magnolia";
+      billingPlan.name !== addon.name ? "" : "border-PurplishBlue bg-Magnolia";
 
     return (
       <div
         key={index}
-        className={`billing-plan ${activePlanStyle}`}
+        className={`billing-plan ${activePlanStyle} py-3`}
         onClick={() => selectAddOn(index)}
       >
-        <img className="max-w-[2.5rem]" src={plan.icon} alt={plan.name} />
-        <div className="flex flex-col">
-          <h2 className="font-bold leading-none capitalize">{plan.name}</h2>
-          <span className="text-CoolGray font-medium">
-            ${plan.priceMonth * priceCoefficent}/mo
+        <div className="flex gap-2 items-center">
+          <div className="shrink-0 w-5 h-5 border border-spacing-1 border-CoolGray rounded-[.27rem]"></div>
+          <div>
+            <h2 className="font-bold leading-5 capitalize">{addon.title}</h2>
+            <span className="text-CoolGray font-medium text-xs">
+              {addon.subtitle}
+            </span>
+          </div>
+          <span className="text-sm shrink-0">
+            +{addon.monthlyPrice * priceCoefficent}/{isYearly ? "yr" : "mo"}
           </span>
-          {freeMonthsElement}
         </div>
       </div>
     );
