cfcc24f436d798c641e3527a75aa89c7374dc176
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-01-26 18:30:32 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { useEffect } from "react";
 
 function App() {
   const [step, setStep] = useState(0);
-  const [darkTheme, setDarkTheme] = useState(true);
+  const [darkTheme, setDarkTheme] = useState(false);
 
   useEffect(() => {
     if (darkTheme == true) document.body.classList.add("dark-theme");
